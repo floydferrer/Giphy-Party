b8941934750259f69e8b4634deada836ea1535fc
Floyd Ferrer <floydferrer@hotmail.com>
2023-11-14 02:09:26 -0800

    created working giphy party

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Let's get this party started!");
+const searchForm = document.getElementById('searchForm');    
+const giphyList = document.getElementById('giphyList');
+const searchTerm = document.getElementById('searchTerm');
+const searchGiphy = document.getElementById('searchGiphy');
+const removeImages = document.getElementById('removeImages');
+
+function createGiphy(giphy) {
+    const newCol = document.createElement('div');
+    newCol.classList.add('col-4');
+    const newGiphy = document.createElement('img');
+    const randomIdx = Math.floor(Math.random()*giphy.data.data.length);
+    newGiphy.classList.add('w-100');
+    newGiphy.src = giphy.data.data[randomIdx].images.original.url;
+    newCol.appendChild(newGiphy);
+    giphyList.appendChild(newCol);
+}
+ 
+searchGiphy.addEventListener('click', async function(event){
+    event.preventDefault();
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', { params: {q: searchTerm.value, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'} });
+    createGiphy(res);
+    searchTerm.value = '';
+}); 
+
+removeImages.addEventListener('click', function(event){
+    event.preventDefault();
+    giphyList.innerHTML = ''
+});
+
+
